bed38e94d12aa36e86bc5887728a611884bd1218
irfakmy <irfakmaulana05@gmail.com>
2025-03-03 22:34:16 +0700

    Add initial implementation of AR model viewer and blur text component

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,88 @@
-import Image from "next/image";
+"use client";
+import Lanyard from './components/Lanyard/Lanyard'
+import RotatingText from "./components/RotatingText/RotatingText";
+import BlurText from './components/BlurText/BlurText';
+import Particles from './components/Particles/Particles';
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const handleAnimationComplete = () => {
+    console.log('Animation completed!');
+  };
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  return (
+    <div className="min-h-screen overflow-x-hidden bg-[#100948] flex items-center justify-center px-4">
+        <div className="absolute inset-0 w-full h-full">
+          <Particles
+            particleColors={['#8F86D9', '#ffffff']}
+            particleCount={2000}
+            particleSpread={8}
+            speed={0.1}
+            particleBaseSize={50}
+            moveParticlesOnHover={true}
+            alphaParticles={false}
+            disableRotation={false}
+          />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <div className="container mx-auto ">
+        <div className="grid grid-cols-1 md:grid-cols-12 h-screen items-center">
+          {/* Bagian Kiri - Teks */}
+          <div className="md:col-span-6 flex flex-col justify-center items-center md:items-start h-full  p-4 md:pl-8">
+            <div className="flex flex-col gap-1 text-center md:text-left">
+              {/* Nama */}
+              <BlurText
+                text="I'm Muhammad Irfa' Maulana"
+                delay={400}
+                animateBy="words"
+                direction="top"
+                onAnimationComplete={handleAnimationComplete}
+                className="text-3xl md:text-4xl font-bold mb-4 md:mb-8 text-white truncate max-w-full whitespace-nowrap overflow-hidden"
+              />
+              {/* Profesi */}
+              <BlurText
+                text="Backend Development"
+                delay={200}
+                animateBy="words"
+                direction="top"
+                onAnimationComplete={handleAnimationComplete}
+                className="text-3xl md:text-4xl font-bold mb-4 md:mb-8 text-[#8F86D9]"
+              />
+            </div>
+            {/* Rotating Text */}
+            <div className="flex flex-col md:flex-row items-center gap-2 md:gap-3">
+              <h1 className="text-lg md:text-1xl text-white font-bold text-center md:text-left">
+                I'm Ready For Job
+              </h1>
+              <RotatingText
+                texts={['Web Programming', 'Web Development', 'Junior Nextjs', 'Laravel Fullstack']}
+                mainClassName="px-2 sm:px-2 md:px-3 bg-[#8F86D9] text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg text-xl md:text-2xl font-bold inline-flex transition-all"
+                staggerFrom={"last"}
+                initial={{ y: "100%" }}
+                animate={{ y: 0 }}
+                exit={{ y: "-120%" }}
+                staggerDuration={0.025}
+                splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+                transition={{ type: "spring", damping: 30, stiffness: 400 }}
+                rotationInterval={2000}
+              />
+            </div>
+              <div className="mt-6 relative z-20">
+              <a href="mailto:irfakmaulana05@gmail.com" target="_blank" rel="noopener noreferrer">
+                <button
+                  className="relative z-20 px-6 py-3 bg-[#8F86D9] text-white text-lg font-semibold rounded-lg shadow-md hover:bg-[#7a72c0] transition-all duration-300"
+                >
+                  Contact Me
+                </button>
+              </a>
+              </div>
+          </div>
+
+          {/* Bagian Kanan - Lanyard */}
+          <div className="md:col-span-6 flex justify-center items-center">
+            <Lanyard position={[0, 0, 17]} gravity={[0, -40, 0]} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
